7ca5cac0bfa3c2c2e1bfd4556ceec02c6d1f580a
troncy_l <lucas.troncy@epitech.eu>
2016-01-08 20:26:57 +0000

    step one structured

---
+++ step_1/include/main.h
@@ -0,0 +1,25 @@
+/*
+** main.h for main header file in /home/troncy_l/prog/PSU_2015_minishell1/include
+** 
+** Made by 
+** Login   <troncy_l@epitech.net>
+** 
+** Started on  Thu Jan  7 20:56:14 2016 
+** Last update Fri Jan  8 20:26:37 2016 
+*/
+
+#ifndef MAIN_H_
+# define MAIN_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct		s_header
+{
+  int			header;
+}			t_header;
+
+#endif /* !MAIN_H_ */
---
+++ step_1/src/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for main function in /home/troncy_l/prog/Rush1/step_1
+** 
+** Made by 
+** Login   <troncy_l@epitech.net>
+** 
+** Started on  Fri Jan  8 20:23:09 2016 
+** Last update Fri Jan  8 20:23:34 2016 
+*/
+
+#include "main.h"
+
+int		main(int argc, char **argv)
+{
+  return (0);
+}
