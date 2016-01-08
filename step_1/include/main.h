50a74a6e377b1ba1488a5705a49f57c49ffbafda
troncy_l <lucas.troncy@epitech.eu>
2016-01-08 21:06:16 +0000

    header Done it works

--- step_1/include/main.h
+++ step_1/include/main.h
@@ -5,13 +5,14 @@
 ** Login   <troncy_l@epitech.net>
 ** 
 ** Started on  Thu Jan  7 20:56:14 2016 
-** Last update Fri Jan  8 20:26:37 2016 
+** Last update Fri Jan  8 21:02:32 2016 
 */
 
 #ifndef MAIN_H_
 # define MAIN_H
 
 # include <stdlib.h>
+# include <stdio.h>
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -19,7 +20,23 @@
 
 typedef struct		s_header
 {
-  int			header;
+  char			name[100];
+  char			mode[8];
+  char			uid[8];
+  char			gid[8];
+  char			size[12];
+  char			mdate[8];
+  char			chksum[8];
+  char			type;
+  char			link[100];
+  char			magic[6];
+  char			version[2];
+  char			uname[32];
+  char			gname[32];
+  char			devmajor[8];
+  char			devminor[8];
+  char			prefix[155];
+  char			end[12];
 }			t_header;
 
 #endif /* !MAIN_H_ */
--- step_1/src/main.c
+++ step_1/src/main.c
@@ -5,12 +5,26 @@
 ** Login   <troncy_l@epitech.net>
 ** 
 ** Started on  Fri Jan  8 20:23:09 2016 
-** Last update Fri Jan  8 20:23:34 2016 
+** Last update Fri Jan  8 21:04:52 2016 
 */
 
 #include "main.h"
 
 int		main(int argc, char **argv)
 {
+  int		fd;
+  int		len;
+  t_header	header;
+
+  if (argc != 2)
+    {
+      printf("wrong Usage\n");
+      return (0);
+    }
+  fd = open(argv[1], O_RDONLY);
+  len = read(fd, &header, 512);
+  printf("READED:%d\n", len);
+  printf("FileName:%s\n", header.name);
+  printf("La taille du 1er fichier est:%s", header.size);
   return (0);
 }
