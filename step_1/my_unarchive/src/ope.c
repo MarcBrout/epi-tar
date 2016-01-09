6282d8438dc78dd26aa2e51907769cba158f290b
troncy_l <lucas.troncy@epitech.eu>
2016-01-09 21:41:52 +0000

    tmp

--- step_1/my_unarchive/src/ope.c
+++ step_1/my_unarchive/src/ope.c
@@ -5,7 +5,7 @@
 ** Login   <troncy_l@epitech.net>
 ** 
 ** Started on  Fri Jan  8 21:13:56 2016 
-** Last update Sat Jan  9 08:08:26 2016 
+** Last update Sat Jan  9 21:15:23 2016 
 */
 
 #include "main.h"
@@ -16,6 +16,7 @@ int		my_untar(int fd)
   int		len;
   char		*tmp;
   int		fa;
+  char		c;
 
   while ((len = read(fd, &header, 512)) != 0)
     {
@@ -24,25 +25,31 @@ int		my_untar(int fd)
       if (len != 512 && (header.name == NULL || header.name[0] == '\0'))
 	return (0);
       printf("Name:%s\n", header.name);
-      if (header.name[strlen(header.name) - 1] == '/')
+      printf("TYPE:%c\n", header.type[0]);
+      printf("Size:%d\n", (int)strtol(header.size, NULL, 10));
+      if (header.type[0] == '5' || header.name[strlen(header.name) - 1] == '/')
 	{
 	  if (mkdir(header.name, S_IRWXU | S_IRWXG | S_IRWXO) != 0)
 	    printf("ERRRROOOOOOOOR\n");
+	  printf("MKDIR HERE\n");
 	}
       else
 	{
-	  printf("Size:%d\n", (int)strtol(header.size, NULL, 10));
 	  fa = creat(header.name, strtol(header.mode, NULL, 8));
+	  if (fa == -1)
+	    printf("Failed to Create file\n");
 	  tmp = malloc(sizeof(char) * (int)strtol(header.size, NULL, 10));
 	  if (tmp == NULL)
 	    printf("FFFFFF\n");
 	  len = read(fd, tmp, (int)strtol(header.size, NULL, 10));
+	  printf("READED:%d\n", len);
 	  write(fa, tmp, len);
-	  printf("CheckSum:%d\n", (int)strtol(header.chksum, NULL, 10));
 	  free(tmp);
 	  close(fa);
-	  read(fd, &header, 512 - (int)strtol(header.size, NULL, 10) % 512);
+	  if (strtol(header.size, NULL, 10) != 0)
+	    read(fd, &header, 512 - (int)strtol(header.size, NULL, 10) % 512);
 	  }
+      read(0, &c, 1);
     }
   return (0);
 }
