49a082d8ea50e534ff373de5b9867b6bd8341d8f
troncy_l <lucas.troncy@epitech.eu>
2016-01-10 10:33:06 +0000

    added -t option

--- step_3/include/my_unarchive.h
+++ step_3/include/my_unarchive.h
@@ -5,13 +5,13 @@
 ** Login   <troncy_l@epitech.net>
 ** 
 ** Started on  Sun Jan 10 06:51:46 2016 
-** Last update Sun Jan 10 07:03:38 2016 
+** Last update Sun Jan 10 10:27:45 2016 
 */
 
 #ifndef MY_UNARCHIVE_H_
 # define MY_UNARCHIVE_H_
 
-int		my_untar(int fd);
+int		my_untar(int fd, int args[]);
 void		disp_tar(int fd);
 
 #endif /* !MY_UNARCHIVE_H_ */
--- step_3/src/args.c
+++ step_3/src/args.c
@@ -5,7 +5,7 @@
 ** Login   <troncy_l@epitech.net>
 ** 
 ** Started on  Sun Jan 10 01:42:48 2016 
-** Last update Sun Jan 10 10:07:05 2016 marc brout
+** Last update Sun Jan 10 10:32:20 2016 
 */
 
 #include "main.h"
@@ -27,7 +27,7 @@ int		check_conflict(int args[], char file[])
       disp_err(MISSING_ARCHIVE);
       return (-1);
     }
-  else if (args[0] == args[1])
+  else if (args[0] == args[1] && args[0] == args[4])
     {
       (args[0] == 0) ? disp_err(NEED_X_OR_C) : disp_err(CONFLICT_XC);
       return (-1);
