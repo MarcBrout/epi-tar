81125b57a0a37afa48dda0ac87e9526b2570dcbf
troncy_l <lucas.troncy@epitech.eu>
2016-01-10 08:25:45 +0000

    step 3 finished, the tar binary is compiled

--- step_3/src/main.c
+++ step_3/src/main.c
@@ -5,7 +5,7 @@
 ** Login   <troncy_l@epitech.net>
 ** 
 ** Started on  Sun Jan 10 01:30:11 2016 
-** Last update Sun Jan 10 07:49:25 2016 
+** Last update Sun Jan 10 08:18:28 2016 
 */
 
 #include "main.h"
@@ -14,22 +14,21 @@ int		main(int argc, char **argv)
 {
   int		args[6];
   int		i;
-  char		file[100];
+  int		index;
 
   if (argc == 1)
     {
       disp_err(NO_ARGS);
       return (-1);
     }
-  file[99] = '\0';
   i = 0;
   while (i < 6)
     args[i++] = 0;
-  if (my_parser(argc, argv, args, file) == -1)
+  if (my_parser(argc, argv, args, &index) == -1)
     {
       write(2, ":(\n", 3);
       return (1);
     }
-  my_start();
+  my_start(args, &argv[index]);
   return (0);
 }
